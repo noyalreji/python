5fe45fae7134f9d4290d5eba84789a8edd76abac
Noyal Jacob <johndoe@example.comNoyal>
2021-07-05 00:00:05 -0500

    2 basic algo

---
+++ jav.py
@@ -0,0 +1,17 @@
+# 1. Print 1-255
+# Print1To255()
+# Print all the integers from 1 to 255. 
+
+for print1to255 in range (1,255 + 1):
+    print(print1to255)
+
+
+# 2. Print Odds 1-255
+# PrintOdds1To255()
+# Print all odd integers from 1 to 255. 
+
+for print1to255 in range (1,255 + 1):
+  if print1to255 % 2 != 0:
+    print(print1to255, end = " ")
+
+
